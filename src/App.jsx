e7ba967433020a51612511e3604aba966a503338
mischiefluis <loidatolentino358@gmail.com>
2023-12-21 00:36:30 +0800

    add fonts and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl">The Box</h1>
+    <div className="w-screen h-screen bg-cover bg-[url('/assets/desktop-bg.png')] p-4 ring-[8px] lg:ring-[12px] xl:ring-[16px] ring-black ring-inset">
+      <div className="max-w-[1140px] mx-auto flex justify-between align-center">
+        <h1 className="text-4xl font-bold">The Box</h1>
+      </div>
     </div>
   )
 }
