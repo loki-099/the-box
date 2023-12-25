32ee0cddcaa0c84d4f35f5170128b80ab2246718
mischiefluis <loidatolentino358@gmail.com>
2023-12-25 20:39:57 +0800

    404 custom page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-cover bg-[url('/assets/desktop-bg.png')] p-4 ring-[8px] lg:ring-[12px] xl:ring-[16px] ring-black ring-inset">
+    <div className="w-screen h-screen bg-cover bg-[url('/assets/desktop-bg.png')] p-4 ring-[8px] lg:ring-[12px] xl:ring-[16px] ring-black ring-inset relative">
       <div className="max-w-[1140px] mx-auto">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<NotFound/>}/>
           </Routes>
         </BrowserRouter>
       </div>
