cba88db7f7d61f435a7c2474285755e956eb71de
mischiefluis <loidatolentino358@gmail.com>
2023-12-27 00:28:11 +0800

    slide menu animations and navs

--- src/components/SlideMenu.jsx
+++ src/components/SlideMenu.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const SlideMenu = ( {isOpen, setOpen} ) => {
-  let display
-  let animation
-  display = isOpen ? 'fixed' : 'hidden'
-  animation = isOpen ? 'slide' : 'right-0'
-  console.log(display);
+  let before = isOpen ? 'before:bottom-0 before:opacity-75' : ''
+  let sliding = isOpen ? 'right-0' : 'right-[-100%]'
 
   const hideMenu = () => {
     setOpen(!isOpen)
   }
 
   return (
-    <div className={`${display} fadeIn top-0 right-0 bottom-0 left-0 bg-black/50`}>
-      <div className={`bg-black fixed top-0 bottom-0 ${animation} w-[50%]`}>
+    <div className={`bg-black fixed transition-all duration-500 top-0 bottom-0 ${sliding} w-[50%] before:fixed before:transition-opacity before:duration-300 before:top-0 before:opacity-0 ${before} before:right-0 before:left-0 before:bg-black before:-z-10 `}>
       <button onClick={hideMenu}><img src="/assets/close.svg" alt="" className='w-[24px] absolute right-4 top-6 z-20'/></button>
-
-      </div>
-
+      <nav>
+        <ul className='flex flex-col items-center gap-8 mt-16'>
+          <NavLink to="/" className='uppercase font-bold text-xl hover:bg-black hover:text-white transition-all ease-out duration-150' onClick={hideMenu}>Home</NavLink>
+          <NavLink to="/about" className='uppercase font-bold text-xl bg-black text-white transition-all ease-out duration-150'>About</NavLink>
+          <NavLink to="/blogs" className='uppercase font-bold text-xl bg-black text-white transition-all ease-out duration-150'>Blogs</NavLink>
+          <NavLink to="/socials" className='uppercase font-bold text-xl bg-black text-white transition-all ease-out duration-150'>Socials</NavLink>
+        </ul>
+      </nav>
     </div>
   )
 }
