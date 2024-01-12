d5a16fb77bfda38a49738e0658b9ac73acfb286b
mischiefluis <loidatolentino358@gmail.com>
2024-01-12 23:56:20 +0800

    font size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import Header from '../components/Header';
 const Home = () => {
   return (
     <div className='lg:pt-4'>
+      <div className='flex flex-col gap-48 flex justify-between'>
+        <h1 className='uppercase font-bold text-5xl text-center leading-[72px]'>Let's see what's inside <br/>the <span className='bg-primaryBlack text-primaryWhite px-2'>box</span></h1>
+      </div>
 
     </div>
   )
