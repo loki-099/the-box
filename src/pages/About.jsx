64fcd4edb907f696dcc24e049513e371ee99f5ba
mischiefluis <loidatolentino358@gmail.com>
2024-01-25 00:07:37 +0800

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='pt-24 w-[95%] max-w-[1140px] mx-auto'>
-      <h1 className='text-4xl font-bold'>About</h1>
+    <div className='pt-32 w-[95%] min-w-[335px] tablet:w-[95%] tablet:max-w-[696px] laptop:max-w-[754px] mx-auto pb-36'>
+      <div>
+        <div>
+          <h1 className='heading-1 uppercase'>The Box</h1>
+          <p className='whitespace-pre-line'>
+            The Box is a passion project of mine that showcases the journals I authored. Its objective is to keep me active at all times and to constantly practice my design and web development abilities as I learn. I understand that the design and user experience are not optimal but I promise that it will improve with time. 
+            
+            Although the content may not be relevant to you, I promised that you would acquire some insight from my perspective. We all have distinct journeys and paths to take, but we are united because we all want to succeed.
+          </p>
+        </div>
+
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
