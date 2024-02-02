c9c3bf710865b96e65f4a5f60543dbe49f84a1ef
mischiefluis <loidatolentino358@gmail.com>
2024-02-02 15:04:14 +0800

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Header from './components/Header'
 import About from './pages/About'
 import Blogs from './pages/Blogs'
 import Socials from './pages/Socials'
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useRef } from 'react'
 
 function App() {
+  const body = useRef()
+
   const location = useLocation()
   // Scroll to top if path changes
   useLayoutEffect(() => {
-    document.getElementById('body').scrollTo(0, 0)
+    body.current.scrollTo(0,0)
   }, [location.pathname])
 
   return (
     <div
       className='w-screen h-screen bg-primaryWhite relative overflow-y-auto overflow-x-hidden'
       id='body'
+      ref={body}
     >
       <div className='w-[95%] min-w-[335px] tablet:max-w-[696px] laptop:max-w-[960px] desktop:max-w-[1140px] mx-auto relative'>
         <Header />
