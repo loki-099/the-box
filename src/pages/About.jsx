3cf4028f0a4dfaa04ce39a3dbed1e5d98c45767b
mischiefluis <loidatolentino358@gmail.com>
2024-02-02 15:10:32 +0800

    fix about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className='pt-32 w-[95%] min-w-[335px] tablet:w-[95%] tablet:max-w-[696px] laptop:max-w-[960px] desktop:max-w-[1140px] mx-auto pb-36'>
       <div className='pt-8 mb-32 laptop:mb-52 laptop:relative laptop:flex laptop:items-center laptop:gap-8 desktop:gap-0 desktop:justify-between'>
-        <div className='min-w-[590px] w-[590px] desktop:w-[560px] desktop:min-w-[560px]'>
+        <div className='desktop:w-[560px] desktop:min-w-[560px]'>
           <h1 className='heading-1 uppercase mb-5'>The Box</h1>
           <p className='body-about'>
             The Box is a passion project of mine that showcases the journals I
